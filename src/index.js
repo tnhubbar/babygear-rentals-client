64942d9af6a1fa0cdfa6120c629982a068a41967
tnhubbar <taryn.johnson919@gmail.com>
2020-10-10 12:48:15 -0400

    Added Store and containers

--- src/index.js
+++ src/index.js
@@ -4,18 +4,27 @@ import './index.css';
 import App from './App';
 import {createStore} from 'redux'
 import {applyMiddleware} from 'redux'
+import {compose} from 'redux'
 import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(reducer, composeEnhancers(
+  applyMiddleware(thunk)
+));
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store = {store}>
     <App />
-  </React.StrictMode>,
+    </Provider>
+    </React.StrictMode>
+  ,
   document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
---
+++ src/reducers/familyReducer.js
@@ -0,0 +1,6 @@
+
+
+export default function familyReducer(state = {families: []} ){
+
+
+}
