cc98a34db9c28ab9ee7074955b172c07d61ba8d9
tnhubbar <taryn.johnson919@gmail.com>
2020-10-10 10:07:56 -0400

    added redux

--- src/App.js
+++ src/App.js
@@ -2,25 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component{ 
+
+  componentDidMount(){
+    fetch('http://localhost:3000/rental_items')
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+  }
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      This will be my project 
     </div>
   );
-}
+}}
 
 export default App;
