52a3f3b28ab5ead1bab9383afabbf5be1790375b
tnhubbar <taryn.johnson919@gmail.com>
2020-12-13 20:16:20 -0500

    added Contact Component

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Contact = () => {
+    return(
+        <div className="contact">
+           <p> If you'd like to get in contact with us, you have several options. You can contact us by phone, email, on social media platforms. 
+               Phone: 777-832-3323
+               Email: BabygearRentals@bgr.com 
+               Facebook: BabyGearRentals4You
+            </p>
+        </div>
+    )
+}
+
+export default Contact;
