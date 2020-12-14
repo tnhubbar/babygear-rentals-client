465e72cad307744ffb30c0cc627a5ca8e8020238
tnhubbar <taryn.johnson919@gmail.com>
2020-12-13 20:18:16 -0500

    Added Contact route to container

--- src/containers/FamiliesContainer.js
+++ src/containers/FamiliesContainer.js
@@ -7,6 +7,7 @@ import {fetchFamilies} from '../actions/fetchFamilies.js'
 import {Route, Switch} from 'react-router-dom'
 import NavBar from '../components/NavBar.js'
 import About from '../components/About.js'
+import Contact from '../components/Contact.js'
 
 
 class FamiliesContainer extends React.Component{
@@ -20,6 +21,7 @@ class FamiliesContainer extends React.Component{
             <div>
                 <Switch>
                 <Route path='/families/new' component={FamilyForm} />
+                <Route path='/families/contact' component={Contact} />
                 <Route path='/families/:id'  render={(routerProps) => <FamilyShow {...routerProps} families={this.props.families}/> } />
                 <Route exact path='/families' render={() => <FamiliesIndex families={this.props.families}/> } />
                 <Route path='/' component={About} />
