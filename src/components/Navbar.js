a08857c88d32c1e81844fe0383b2ec7b2bbc6219
tnhubbar <taryn.johnson919@gmail.com>
2020-12-13 20:26:06 -0500

    Added the option to view by location

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,7 @@ const NavBar = () => {
         <div className="navbar" style={{backgroundColor: "white"}} >
             <NavLink to="/">Home</NavLink><br/>
             <NavLink to="/families" >List of Families</NavLink><br/>
+            <NavLink to="/families/locations" >List of Locations</NavLink><br/>
             <NavLink to="/families/new">New Family</NavLink><br/>
             <NavLink to="/families/contact">Contact Us</NavLink><br/>
 
